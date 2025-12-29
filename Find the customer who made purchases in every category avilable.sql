c721d247dabc4f43fcbba1c1b847b9dab6c93700
Omkar Ghodake <uniquegalaxy2000@gmail.com>
2025-12-30 00:40:07 +0530

    Create Find the customer who made purchases in every category avilable.sql

---
+++ Find the customer who made purchases in every category avilable.sql
@@ -0,0 +1,82 @@
+-- Find the customer who made purchases in every category avilable'
+
+
+CREATE TABLE categories (
+    category_id INT PRIMARY KEY,
+    category_name VARCHAR(50)
+);
+
+
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(50),
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+);
+
+
+CREATE TABLE sales_items (
+    item_id INT PRIMARY KEY, 
+    sales_id BIGINT,
+    product_id INT,
+    user_id BIGINT,
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+
+
+INSERT INTO categories (category_id, category_name) VALUES 
+(1, 'Electronics'),
+(2, 'Software'),
+(3, 'Consulting');
+
+
+INSERT INTO products (product_id, product_name, category_id) VALUES 
+(10, 'Laptop', 1), 
+(11, 'Phone', 1), 
+(20, 'SaaS Sub', 2),
+(21, 'Antivirus', 2),
+(30, 'Strategy HR', 3),
+(31, 'Tech Support', 3);
+
+
+INSERT INTO sales_items (item_id, sales_id, product_id, user_id) VALUES
+(1, 1, 10, 3333),  
+(2, 1, 20, 2222),  
+(3, 1, 30, 4444),  
+(4, 2, 10, 2222),  
+(5, 2, 11, 3333),  
+(6, 2, 20, 4444),  
+(7, 4, 30, 2222),  
+(8, 5, 31, 1111),  
+(9, 3, 10, 3333),  
+(10, 7, 21, 7777), 
+(11, 3, 11, 4444), 
+(12, 4, 10, 4444); 
+
+
+with cte1 as (
+  select 
+    a.user_id,
+    a.sales_id,
+    a.item_id,
+    a.product_id,
+    b.product_name,
+    b.category_id,
+    c.category_name
+  from 
+    sales_items as a 
+  left join products as b 
+  on 
+    a.product_id = b.product_id
+  left join 
+    categories as c 
+  on 
+    b.category_id = c.category_id
+)
+
+select 
+  user_id,
+  count(distinct category_id) as total_category
+from  cte1
+group by user_id
+having total_category = ( select count(distinct category_id) from categories) ;
